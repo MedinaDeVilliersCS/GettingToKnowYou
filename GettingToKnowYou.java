9b636db7a8bea543489827ae18b0a9bf3e798ad6
Catherine Medina-DeVilliers <catherine.medina-devilliers@acps.k12.va.us>
2020-09-10 22:12:12 +0000

    Initial commit

---
+++ GettingToKnowYou.java
@@ -0,0 +1,18 @@
+
+public class GettingToKnowYou
+{
+
+public static void helloFromMrsMD()
+{
+  System.out.println("\n Hello and my favorite food is arroz con gandules.");
+
+}
+
+public static void main()
+{
+  System.out.println("Hello!");
+  helloFromMrsMD();
+
+}
+
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+
+//package GettingToKnowYou;
+class Main
+{
+public static void main(String[] args)
+ {
+  GettingToKnowYou.main();
+ }
+
+}
